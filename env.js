a1e5771626d588d2de4bed49eb02b0b17d270244
Cauê dos Santos Silva <cauedossantossilva@hotmail.com>
2020-04-01 16:04:20 -0300

    feat: Configuração do Reactotron

---
+++ env.js
@@ -0,0 +1,3 @@
+export default {
+  HOST: '192.168.1.3',
+};
---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,17 @@
+import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
+
+import ENV from '../../env';
+
+if (__DEV__) {
+  const tron = Reactotron.configure({ host: ENV.HOST })
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
