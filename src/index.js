8238bd9483c6e38657ca312e928d763dab048249
Cauê dos Santos Silva <cauedossantossilva@hotmail.com>
2020-04-01 15:29:51 -0300

    feat: Estilizando StatusBar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './routes';
@@ -7,6 +8,7 @@ import Routes from './routes';
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <Routes />
     </NavigationContainer>
   );
